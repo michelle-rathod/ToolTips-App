ddedb0409b17e1baccc83850f683da20bd150a62
michelle-rathod <rathodmichelle@gmail.com>
2022-09-28 19:04:24 +0530

    first commit

---
+++ build/precache-manifest.1a47bc1803da0f61f307566941ce7637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "428af45e39159d9d974e",
+    "url": "/static/css/main.7213cc83.chunk.css"
+  },
+  {
+    "revision": "428af45e39159d9d974e",
+    "url": "/static/js/main.428af45e.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "4c6d4b0041a5f8cc2933",
+    "url": "/static/js/2.4c6d4b00.chunk.js"
+  },
+  {
+    "revision": "4431f52a260a7d5c73e4",
+    "url": "/static/js/runtime~main.4431f52a.js"
+  },
+  {
+    "revision": "796fafcdb4e3392532576a2227c8f673",
+    "url": "/index.html"
+  }
+];
